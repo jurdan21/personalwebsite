8fa632c3e2977a3f002216c550e289866d54852a
jurdan21 <jurdan.aho@gmail.com>
2026-06-19 09:17:35 +0700

    update new

--- src/app/FooterSection.tsx
+++ src/app/FooterSection.tsx
@@ -10,7 +10,7 @@ export default function FooterSection() {
           >
             LET&apos;S START<br className="hidden md:block" /> SOMETHING
           </h4>
-          <a href="mailto:hello@jurdanwahyu.com" className="text-[20px] md:text-[40px] leading-[1.1] font-normal md:self-end relative after:content-[''] after:block after:h-[2px] after:bg-white after:scale-x-0 after:transition-transform after:duration-300 after:origin-left hover:after:scale-x-100">hello@jurdanwahyu.com</a>
+          <a href="mailto:jurdann.wahyu@gmail.com" className="text-[20px] md:text-[40px] leading-[1.1] font-normal md:self-end relative after:content-[''] after:block after:h-[2px] after:bg-white after:scale-x-0 after:transition-transform after:duration-300 after:origin-left hover:after:scale-x-100">jurdann.wahyu@gmail.com</a>
         </div>
         {/* Row 2 */}
         <div className="flex flex-col md:flex-row items-start md:items-start justify-between w-full md:w-1/2 px-4 md:px-8 mx-auto gap-6 md:gap-0">
@@ -21,7 +21,7 @@ export default function FooterSection() {
             <a href="https://www.instagram.com/jurdanwahyu/" className="relative after:content-[''] after:block after:h-[2px] after:bg-white after:scale-x-0 after:transition-transform after:duration-300 after:origin-left hover:after:scale-x-100">Instagram</a>
             <a href="https://medium.com/@jurdann.wahyu" className="relative after:content-[''] after:block after:h-[2px] after:bg-white after:scale-x-0 after:transition-transform after:duration-300 after:origin-left hover:after:scale-x-100">Medium</a>
           </div>
-          <a href="https://drive.google.com/file/d/1LnTd3J2nkCjroVenXbjyyE0i2amSDXn2/view?usp=sharing" className="text-[16px] md:text-[24px] mt-4 md:mt-0 relative after:content-[''] after:block after:h-[2px] after:bg-white after:scale-x-0 after:transition-transform after:duration-300 after:origin-left hover:after:scale-x-100">Resume ↓</a>
+          <a href="https://drive.google.com/drive/folders/1OU0F8Rt_7BAcQSNkXsbx5dI0rqAZD3wL" className="text-[16px] md:text-[24px] mt-4 md:mt-0 relative after:content-[''] after:block after:h-[2px] after:bg-white after:scale-x-0 after:transition-transform after:duration-300 after:origin-left hover:after:scale-x-100">Resume ↓</a>
         </div>
         {/* Row 3 */}
         <div className="flex flex-col md:flex-row items-center justify-between w-full px-4 md:px-8 text-[12px] md:text-[14px] gap-2 md:gap-0">
